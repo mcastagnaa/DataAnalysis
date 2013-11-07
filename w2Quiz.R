ccab5ed29523803e3f7b65666c84cdcc58fe9b58
Matteo Castagna <matteo.castagna@gmail.com>
2013-11-07 13:51:03 +0000

    EndOfQuiz2
    Material for Quiz on w2
    loandCodebook is for the Data Assignment1

--- w2Quiz.R
+++ w2Quiz.R
@@ -12,10 +12,11 @@ url <- "https://spark-public.s3.amazonaws.com/dataanalysis/ss06hid.csv"
 download.file(url, "./downloads/w2quiz3.csv", method="curl")
 con <-file("./downloads/w2quiz3.csv")
 USCommData <- read.csv(con)
+close(con)
 #3
 print(table(USCommData$VAL)[24])
 #4
-USCommData$FES
+#USCommData$FES
 #5
 table(USCommData$BDS, USCommData$RMS)
 #6 ACR = 3, AGS = 6  
@@ -25,3 +26,16 @@ which(agricultureLogical)
 indexes <- which(agricultureLogical)
 subsetDataFrame  <- USCommData[indexes,] 
 sum(is.na(subsetDataFrame$MRGX))
+#8
+strsplit(names(USCommData), "wgtp")[123]
+#9
+quantile(USCommData$YBL, na.rm=TRUE)
+#10
+url <- "https://dl.dropbox.com/u/7710864/data/csv_hid/ss06pid.csv "
+download.file(url, "./downloads/ss06pid.csv", method="curl")
+con <- file("./downloads/ss06pid.csv")
+populationData <- read.csv(con)
+close(con)
+mergedSet <- merge(USCommData, populationData, "SERIALNO", all = TRUE)
+dim(mergedSet)
+
