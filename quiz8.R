628308834efdd0c8dc0c128583beb0777126e8d2
Matteo Castagna <matteo.castagna@gmail.com>
2014-01-21 11:51:42 +0000

    Final commit

--- quiz8.R
+++ quiz8.R
@@ -16,3 +16,36 @@ control <- alpha*seq(1,100)/100
 sum(pValues < control)
 
 #4
+set.seed(1234)
+e <- rnorm(100)
+x <- rnorm(100)
+b0 <- 1
+b1 <- 2
+y <- b0 + b1 * x + e
+lmbase <- lm(y ~ x)
+summary(lmbase)
+lostDataQntl <- 0.7
+quantile(x, lostDataQntl)
+quantile(y, lostDataQntl)
+
+xHighOut <- x
+xHighOut[xHighOut > quantile(x, lostDataQntl)] <- NA
+
+lmXout <- lm(y ~ xHighOut)
+summary(lmXout)
+
+yHighOut <- y
+yHighOut[yHighOut > quantile(y, lostDataQntl)] <- NA
+
+lmYout <- lm(yHighOut ~ x)
+summary(lmYout)
+
+#5
+library(MASS)
+rlmbase <- rlm(y ~ x)
+rlmXout <- rlm(y ~ xHighOut)
+rlmYout <- rlm(yHighOut ~ x)
+
+summary(rlmbase)
+summary(rlmXout)
+summary(rlmYout)
